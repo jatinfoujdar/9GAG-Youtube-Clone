950fc242e080a963723b809ca661e15097338c29
Jatin Foujdar <jatin321001@gmail.com>
2023-04-21 23:55:34 +0530

    fetching yt video through api commit

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { YT_V } from '../utils/config';
 
 const VideoContainer = () => {
+  useEffect(()=>{
+
+    getVideos();
+  },[])
+
+  const getVideos = async()=>{
+    const data = await fetch(YT_V);
+    const json = await data.json();
+    console.log(json);
+  }
   return (
-    <div>VideoContainer</div>
+    <div>
+
+    </div>
   )
 }
 
