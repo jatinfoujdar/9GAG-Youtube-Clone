618d5bbb8f57386f3367c25cc15edaae6780258b
Jatin Foujdar <jatin321001@gmail.com>
2023-04-26 13:17:11 +0530

    suggestion api commit

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,4 @@
 const Api_key = "AIzaSyAsLMAD0Tp9hXZDNdnhaZMdnxt99eh9t5k";
 export const YT_V = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+ Api_key;
 
+export const SuggestionAPI = "http://suggestqueries.google.com/complete/search?output=toolbar&hl=en&q="
